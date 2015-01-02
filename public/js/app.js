92c98fe9bd23c0ac5492ced2b12437703d080e12
Matt Lea <mlea@schematical.com>
2015-01-02 13:28:31 -0600

    Bananas

---
+++ public/js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module(
+	'wheezy',
+	[
+		'ngCookies'
+    ]
+).config(
+        [
+            '$routeProvider',
+            function($routeProvider) {
+
+        }
+    ]
+)
