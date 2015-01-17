04196e97aa38f9fa585a30f18c54c954367000f9
Matt Lea <mlea@schematical.com>
2015-01-17 17:22:55 -0600

    Serailazing world + saving to mongo

--- app.js
+++ app.js
@@ -8,13 +8,18 @@ app.use(connect_multiparty());
 require('./lib')(app);
 app.use(express.static(__dirname + '/public'));
 
-
-app.game = new app.Game({});
+var config = require('./config');
+app.game = new app.Game({
+	config:config
+});
 io.on('connection', function(socket){ app.game.connect_user(socket) });
 
 
 
 http.listen(3000, function(){
 	console.log('listening on *:3000');
-	app.game.init();
+	app.game.loadWorld(function(err){
+		if(err) throw err;
+		app.game.init();
+	});
 });
